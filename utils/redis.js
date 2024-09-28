9537accc18553d431694690e24906823f2eaeb53
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-28 16:58:02 +0300

    redis utils

---
+++ utils/redis.js
@@ -0,0 +1,45 @@
+const redis = require('redis');
+
+
+class RedisClient {
+    constructor() {
+        this.client = redis.createClient();
+        this.client.on('error', (error) => { console.log(`Redis client not connected to the server: ${error}`); });
+        this.client.on('connect', () => { console.log('Redis client connected to the server'); });
+    }
+
+    isAlive() {
+        return this.client.connected;
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            this.client.get(key, (err, reply) => {
+                if (err) reject(err);
+                resolve(reply);
+            });
+        });
+    }
+
+    async set(key, value, duration) {
+        return new Promise((resolve, reject) => {
+            this.client.setex(key, duration, value, (err, reply) => {
+                if (err) reject(err);
+                resolve(reply);
+            });
+        });
+    }
+
+    async del(key) {
+        return new Promise((resolve, reject) => {
+            this.client.del(key, (err, reply) => {
+                if (err) reject(err);
+                resolve(reply);
+            });
+        });
+    }
+}
+
+const redisClient = new RedisClient();
+redisClient.client.connect();
+module.exports = redisClient;
