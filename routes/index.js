a127a92e8676577e508368863d70def851e38e26
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-30 02:22:06 +0300

    edit

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
 
 const apiRoutes = (api) => {
   api.get('/status', AppController.getStatus);
