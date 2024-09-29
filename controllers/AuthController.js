67a35c3b5f50f6d4397b23a71602eb451a876b59
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-29 23:54:59 +0300

    AuthController

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -4,7 +4,7 @@ import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
 
 export default class AuthController {
-  static getConnect(req, res) {
+  static async getConnect(req, res) {
     const auth = req.headers.authorization;
     if (!auth || !auth.startsWith('Basic ')) return res.status(401).send({ error: 'Unauthorized' });
     const authHeader = auth.split(' ')[1];
@@ -12,20 +12,28 @@ export default class AuthController {
     const [email, password] = authHeaderStr.split(':');
     if (!email || !password) return res.status(401).send({ error: 'Unauthorized' });
     const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-    dbClient.db.collection('users').findOne({ email, password: hashedPassword }, (error, user) => {
-      if (error || !user) return res.status(401).send({ error: 'Unauthorized' });
+    try {
+      const user = await dbClient.db.collection('users').findOne({ email, password: hashedPassword });
+      if (!user) return res.status(401).send({ error: 'Unauthorized' });
       const token = uuidv4();
       redisClient.set(`auth_${token}`, user._id.toString(), 86400);
       return res.status(200).send({ token });
-    });
+    }
+    catch (error) {
+      return res.status(500).send({ error: 'Internal Server Error' });
+    }
   }
 
-  static getDisconnect(req, res) {
+  static async getDisconnect(req, res) {
     const token = req.headers['X-Token'];
     if (!token) return res.status(401).send({ error: 'Unauthorized' });
-    redisClient.del(`auth_${token}`, (_error, response) => {
+    try {
+      const response = await redisClient.del(`auth_${token}`);
       if (response === 1) return res.status(204).send();
       return res.status(401).send({ error: 'Unauthorized' });
-    });
+    }
+    catch (error) {
+      return res.status(500).send({ error: 'Internal Server Error' });
+    }
   }
 }
