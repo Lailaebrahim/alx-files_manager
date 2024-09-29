cb99df569348e9245115653fa0a588873a9270b5
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-29 15:19:46 +0300

    UsersController

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,13 +1,16 @@
 const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
+const crypto = require('crypto');
+
 
 export default class UsersController{
     static postNew(req, res) {
         const { email, password } = req.body;
         if (!email) return res.status(400).send({ error: 'Missing email' });
         if (!password) return res.status(400).send({ error: 'Missing password' });
+        const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
         dbClient.nbUsers().then((nbUsers) => {
-            dbClient.db.collection('users').insertOne({ email, password }, (error, result) => {
+            dbClient.db.collection('users').insertOne({ email, hashedPassword }, (error, result) => {
                 if (error) return res.status(400).send({ error: 'Already exist' });
                 return res.status(201).send({ id: result.insertedId, email });
             });
