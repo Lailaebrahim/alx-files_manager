1679d1bd1d4a0d03ca3f31acf1c82bd057321f63
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-29 17:49:17 +0300

    UsersController & AuthController

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const apiRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
   api.post('/users', UsersController.postNew);
+  api.get('/connect', AuthController.getConnect);
+  api.get('/disconnect', AuthController.getDisconnect);
 };
 
 export default apiRoutes;
