b5ef88022533a126422391259293315670c1b636
lailaebrahim <ebrahimlaila815@gmail.com>
2024-09-29 16:47:33 +0300

    UsersController

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,16 +2,19 @@ const dbClient = require('../utils/db');
 const redisClient = require('../utils/redis');
 
 export default class AppController {
-  static getStatus(req, res) {
+  static getStatus(_req, res) {
     const redis = redisClient.isAlive();
     const db = dbClient.isAlive();
     res.status(200).json({ redis, db });
   }
 
-  static getStats(req, res) {
+  static getStats(_req, res) {
     Promise.all([dbClient.nbUsers, dbClient.nbFiles])
       .then(([Users, nbFiles]) => {
         res.status(200).json({ users: Users, files: nbFiles });
-      });
+      })
+    .catch((_error) => {
+        res.status(500).json({ error: 'Internal Server Error' });
+    });
   }
 }
