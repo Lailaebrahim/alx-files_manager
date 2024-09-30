84c82d6e2afb628d4447486abe96b1c9ed2e82a9
lailaebrahim <ebrahimlaila815@gmail.com>
2024-10-01 01:19:26 +0300

    FilesController.getFile

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -9,6 +9,7 @@ import Queue from 'bull';
 import { createDirectory, convertFromBase64 } from '../utils/file';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+import { contentType } from 'mime-types';
 
 const fileQueue = new Queue('thumbnail generation');
 
@@ -222,27 +223,31 @@ export default class FilesController {
   static async getFile(req, res) {
     try {
       const fileId = req.params.id;
+      // If no file document is linked to the ID passed as parameter, return an error Not found with a status code 404
       const file = await dbClient.db.collection('files').findOne({ _id: ObjectId(fileId) });
       if (!file) return res.status(404).send({ error: 'Not found' });
+      // If the type of the file document is folder, return an error A folder doesn't have content with a status code 400
       if (file.type === "folder") return res.status(400).send({ error: "A folder doesn't have content" });
       if (file.isPublic) {
-        return res.status(200).send({ id: file._id, userId: file.userId, name: file.name, type: file.type, isPublic: file.isPublic, parentId: file.parentId });
+        // If the file is public, return the file
+        const filePath = await fsPromises.realpath(file.localPath);
+        res.setHeader('Content-Type', contentType(file.name) || 'text/plain; charset=utf-8');
+        return res.status(200).sendFile(filePath);
       }
       else {
-        if (file.userId === req.user._id) { return res.status(200).send({ id: file._id, userId: file.userId, name: file.name, type: file.type, isPublic: file.isPublic, parentId: file.parentId }); }
-        else {
-          // get user based on the token
-          const token = req.header('X-Token');
-          if (!token) return res.status(403).send({ error: 'Forbidden' });
-          const userId = await redisClient.get(`auth_${token}`);
-          if (!userId) return res.status(403).send({ error: 'Forbidden' });
-          const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
-          if (!user) return res.status(403).send({ error: 'Forbidden' });
-          if (file.userId !== user._id) return res.status(403).send({ error: 'Forbidden' });
-          return res.status(200).send({ id: file._id, userId: file.userId, name: file.name, type: file.type, isPublic: file.isPublic, parentId: file.parentId });
-        }
+        // get user based on the token
+        const token = req.header('X-Token');
+        if (!token) return res.status(403).send({ error: 'Forbidden' });
+        const userId = await redisClient.get(`auth_${token}`);
+        if (!userId) return res.status(403).send({ error: 'Forbidden' });
+        const user = await dbClient.db.collection('users').findOne({ _id: ObjectId(userId) });
+        if (!user) return res.status(403).send({ error: 'Forbidden' });
+        if (file.userId !== user._id) return res.status(403).send({ error: 'Forbidden' });
+        // If the file is private and the user is the owner of the file, return the file
+        const filePath = await fsPromises.realpath(file.localPath);
+        res.setHeader('Content-Type', contentType(file.name) || 'text/plain; charset=utf-8');
+        return res.status(200).sendFile(filePath);
       }
-
     }
     catch (Error) {
       return res.status(500).send({ error: `Internal Server Error: ${Error}` });
